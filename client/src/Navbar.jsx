d88167d4428b81bc54d0ce842905eb794939577e
Arun Nishad <arunnishad94136@gmail.com>
2024-11-24 01:19:08 +0530

    Update Navbar.jsx

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
   };
 
   return (
+    <>
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         {/* Logo */}
@@ -128,7 +129,8 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
-      <hr /><hr />
+      <hr />
+      </>
   );
 };
 
