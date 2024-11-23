7417511895db4dc14c6d54fab49f884876f0ae58
Arun Nishad <arunnishad94136@gmail.com>
2024-11-24 01:13:27 +0530

    Update SkillsCard.jsx

--- client/src/SkillsCard.jsx
+++ client/src/SkillsCard.jsx
@@ -6,7 +6,7 @@ return (
 <>
 
     <div key={id} className='p-4 mx-2 border-2 border-red-300'>
-        <img className="h-auto max-w-full rounded-lg" height='100' width='100' src={img} alt="tech-images" />
+        <img className="h-auto max-w-full rounded-lg" height='70' width='70' src={img} alt="tech-images" />
     </div>
     </>
   )
